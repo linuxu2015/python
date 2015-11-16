553ec7a8e85577c4a4d43a3caca18e4d8fd1d782
linuxu2015 <linuxu2015@gmail.com>
2015-11-16 20:42:54 +0800

    nothing

--- atm/filetodic.py
+++ atm/filetodic.py
@@ -4,11 +4,11 @@ def filetodic(dicname,path):
     #print userlist
 #    dicname = {}
     for line in userlist.readlines():
-#	print line
-	new_line = line.split()
-#	print new_line
-#	print new_line[1],new_line[0]
-	dicname[new_line[0]] = new_line[1]
+        #print line
+""" new_line = line.split()
+#   print new_line
+# 	print new_line[1],new_line[0]"""
+    dicname[new_line[0]] = new_line[1]
     print dicname
 if __name__ == '__main__':
-	filetodic()
+    filetodic()
